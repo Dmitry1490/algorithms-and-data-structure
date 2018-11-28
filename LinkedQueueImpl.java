923e731a84fdf98b6ed524ef38acf1d2cf3e9f83
Dmitry1490 <Unamas@mail.ru>
2018-11-28 22:33:50 +0300

    Class LinkedQueue

---
+++ LinkedQueueImpl.java
@@ -0,0 +1,33 @@
+public class LinkedQueueImpl  implements LinkedQueue{
+
+    private TwoSideLinkedList linkedList;
+
+    public LinkedQueueImpl() {
+        this.linkedList = new TwoSideLinkedListImpl();
+    }
+
+    @Override
+    public void insert(int value) {
+        linkedList.addLast(value);
+    }
+
+    @Override
+    public int remove() {
+        return linkedList.remove();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return linkedList.isEmpty();
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return linkedList.getSize();
+    }
+}
